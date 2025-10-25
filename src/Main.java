21be25092e430ee34b26285570c661f865ac5d39
jackt <jacktimmons@cmail.carleton.ca>
2025-10-24 20:35:21 -0400

    Error handling

--- src/Main.java
+++ src/Main.java
@@ -12,12 +12,28 @@ public class Main {
         System.out.println("Welcome to Scrabble!");
 
         // Ask for number of players (2-4)
+
+        boolean gettingInput = true; // true while the user is entering input
         int numPlayers = 0;
-        while (numPlayers < 2 || numPlayers > 4) {
+
+        while(gettingInput){
             System.out.print("Enter number of players (2-4): ");
-            numPlayers = scanner.nextInt();
+            try{
+                numPlayers = new Scanner(System.in).nextInt();
+
+                if (numPlayers >= 2 && numPlayers <= 4){    // if the input is 2-4, continue
+                    gettingInput = false;
+                }else{
+                    System.out.println("ERROR: can only be 2-4 players");   // if input is != 2->4, display error
+                }
+
+            }catch(Exception e){
+                System.out.println("ERROR: enter a number");    // if the input != int, display error
+            }
         }
-        scanner.nextLine(); // consume newline
+
+
+
 
         // Create and start game
         Game game = new Game(numPlayers);
